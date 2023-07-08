c43b80edb634e736de4e2b073647d58fb18e8f6b
cesarcuadro <cesarcuadro777@gmail.com>
2023-07-08 14:18:07 -0700

    eslint change

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import { loader } from '../assets';
 
-const Loader = ({ title  }) => (
+const Loader = ({ title }) => (
   <div className="w-full flex justify-center items-center flex-col">
     <image src={loader} alt="loader" className="w-32 h-32 object-contain" />
     <h1 className="font-bold text-2xl text-white mt-2">{title || 'Loading ...'}</h1>
