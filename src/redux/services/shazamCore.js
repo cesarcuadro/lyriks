450a3e896d6269703573903f1dfc5177ddf0d4aa
cesarcuadro <cesarcuadro777@gmail.com>
2023-07-07 19:26:27 -0700

    removed boilerplate and insert in shazamCoreApi

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,19 +3,14 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '16dd8d3f3dmsh758a8a4bf4165acp1624b8jsn1727776d04f3',
-    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
-  },
-};
-
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '16dd8d3f3dmsh758a8a4bf4165acp1624b8jsn1727776d04f3');
+
+      return headers;
+    },
+  }),
 });
