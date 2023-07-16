fcf687181afc5b4180bb35c28a12c62086942fca
cesarcuadro <cesarcuadro777@gmail.com>
2023-07-15 21:16:33 -0700

    sidebar on mobile is functional

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
 
+import { HiOutlineMenu } from 'react-icons/hi';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
@@ -24,10 +25,22 @@ const NavLinks = ({ handleClick }) => (
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileOpen] = useState(false);
   return (
+    <>
     <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
       <img src={logo} alt="logo" className="w-full h-14 object-contain" />
       <NavLinks />
     </div>
+    <div className="absolute md:hidden top-6 right-3">
+      {mobileMenuOpen ? (
+        <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileOpen(false)} />
+      ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileOpen(true)} />
+    }
+    </div>
+    <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      <NavLinks handleClick={() => setMobileOpen(false)} />
+    </div>
+    </>
   );
 };
 
